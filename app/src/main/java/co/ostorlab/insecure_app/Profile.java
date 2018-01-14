9a09b813088f32f27c34cc00644edc235dde28ac
asasas333_3 <asasas333_3@hotmail.com>
2018-01-14 11:53:36 +0100

    Initial commit

---
+++ app/src/main/java/co/ostorlab/insecure_app/Profile.java
@@ -0,0 +1,16 @@
+package co.ostorlab.insecure_app;
+
+/**
+ * Created by asasas333_3 on 14/12/17.
+ */
+
+public class Profile {
+
+    public String name;
+    public int age;
+
+    public Profile(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
