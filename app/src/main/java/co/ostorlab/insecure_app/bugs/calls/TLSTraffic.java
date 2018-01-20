d850a059463625a3495e86a57af1e059af0c6467
asasas333_3 <asasas333_3@hotmail.com>
2018-01-20 22:49:35 +0100

    Add TLSTraffic rule

--- app/src/main/java/co/ostorlab/insecure_app/bugs/calls/TLSTraffic.java
+++ app/src/main/java/co/ostorlab/insecure_app/bugs/calls/TLSTraffic.java
@@ -1,22 +1,35 @@
 package co.ostorlab.insecure_app.bugs.calls;
 
+import android.util.Log;
 import co.ostorlab.insecure_app.BugRule;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-
 public final class TLSTraffic extends BugRule {
 
-
+    private static final String TAG = TLSTraffic.class.toString();
 
     @Override
     public void run() throws Exception {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                             .url("https://www.google.com/")
-                             .build();
-        Response response = client.newCall(request).execute();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient.Builder()
+                            .hostnameVerifier(org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
+                            .build();
+                    Request request = new Request.Builder()
+                            .url("https://www.google.com/")
+                            .build();
+                    Call okhttpCall = client.newCall(request);
+                    Response response = okhttpCall.execute();
+                }
+                catch (Exception e) {
+                    Log.e(TAG, "error initializing TLSTraffic thread", e);
+                }
+            }
+        }).start();
     }
 
     @Override
