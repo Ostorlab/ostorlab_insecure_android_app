1b6dd10287bc0507849aa2b6f7de78f1b5ce27f2
ErebusZ <azouiten@ostorlab.dev>
2023-05-23 14:48:00 +0100

    add bug rule for biometric none crypto object

---
+++ ostorlab_insecure_flutter_app/lib/bugs/biometric_none_cryptobject.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+
+abstract BiometricNoneCryptObject class BugRule {
+
+  static const String _tag = 'BiometricNoneCryptObject';
+
+  String get description => 'Use Biometric authentication without a crypto Object.';
+
+  Future<void> run() async {
+    // Initialising an instance of the localAuthentication.
+    final LocalAuthentication localAuthentication = LocalAuthentication();
+
+    // Attempting authentication using biometrics
+    final isAuth = localAuthentication.authenticate(
+    localizedReason: 'Please complete the biometrics to proceed.');
+  }
+
+}
